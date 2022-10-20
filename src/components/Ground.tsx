6f189fb00c04db1ccc5f4ec71c9be6d1136a54c7
jerry <calljerryli@outlook.com>
2022-10-21 02:43:10 +0800

    groundTexture magFilter

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -1,8 +1,10 @@
+import { groundTexture } from "../images/textures";
+
 export const Ground = () => {
   return (
     <mesh>
-      <planeGeometry/>
-      <meshStandardMaterial color={'red'}/>
+      <planeGeometry args={[100, 100]}/>
+      <meshStandardMaterial map={groundTexture}/>
     </mesh>
   )
 }
--- src/images/textures.ts
+++ src/images/textures.ts
@@ -1,7 +1,11 @@
-import { TextureLoader } from "three";
+import { NearestFilter, RepeatWrapping, TextureLoader } from "three";
 import grassImage from './grass.jpg';
 
 const groundTexture = new TextureLoader().load(grassImage)
+groundTexture.magFilter = NearestFilter
+groundTexture.wrapS = RepeatWrapping
+groundTexture.wrapT = RepeatWrapping
+groundTexture.repeat.set(100, 100)
 
 export {
   groundTexture
