7fb2beb15d936a17e649b69cd18612f511fe8ec5
jerry <calljerryli@outlook.com>
2022-10-21 02:14:15 +0800

    导入贴图

---
+++ src/images/images.ts
@@ -0,0 +1,13 @@
+import dirtImage from './dirt.jpg'
+import glassImage from './glass.png'
+import grassImage from './grass.jpg'
+import logImage from './log.jpg'
+import woodImage from './wood.png'
+
+export {
+  dirtImage,
+  glassImage,
+  grassImage,
+  logImage,
+  woodImage
+}
