a96063448dda1ce67e792a410ee7a8fb3e6aa749
jerry <calljerryli@outlook.com>
2022-10-21 19:17:15 +0800

    direction

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -4,6 +4,9 @@ import { useEffect, useRef } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useKeyboard } from "../hooks/useKeyboard";
 
+const JUMP_FORCE = 4
+const SPEED = 4
+
 export const Player = () => {
   const actions = useKeyboard();
   useEffect(() => {
@@ -37,9 +40,31 @@ export const Player = () => {
     // camera
     camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]))
 
+    // direction
+    // x 1 right
+    // x -1 left
+    // z 1 back
+    // z -1 front
+    const frontDirection = new Vector3(0, 0, 0)
+    if (actions.moveForward) {
+      frontDirection.setZ(-1)
+    } else if (actions.moveBackward) {
+      frontDirection.setZ(1)
+    }
+    const sideDirection = new Vector3(0, 0, 0)
+    if (actions.moveLeft) {
+      sideDirection.setX(-1)
+    } else if (actions.moveRight) {
+      sideDirection.setX(1)
+    }
+    const finalDirection = new Vector3()
+    finalDirection.addVectors(frontDirection, sideDirection)
+    finalDirection.multiplyScalar(SPEED)
+    api.velocity.set(finalDirection.x, vel.current[1], finalDirection.z)
+
     // jump
     if (actions.jump && Math.abs(vel.current[1]) < 0.01) {
-      api.velocity.set(vel.current[0], 10, vel.current[2])
+      api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2])
     }
   })
 
